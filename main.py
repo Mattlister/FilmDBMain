01d534303a3b1474085ad220b6dc8ef8b3459640
Matthew Lister <mdlister24@hotmail.com>
2020-01-11 13:45:32 +0000

    Changes due to navigation issues

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route("/")
+@app.route("index")
 def index ():
     return render_template("index.html")
 
-@app.route("/TV")
+@app.route("/tv")
 def TV():
     return render_template("tv.html")
 
