0d3f83399afdf8f2a313bf893bf7ed1b8dbafb50
Matthew Lister <mdlister24@hotmail.com>
2020-01-11 17:07:02 +0000

    Added new template links

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ app = Flask (__name__)
 def index ():
     return render_template("index.html")
 
+@app.route("/films")
+def index ():
+    return render_template("films.html")    
+
 @app.route("/tv")
 def tv():
     return render_template("tv.html")
 
-@app.route("/Casting")
+@app.route("/casting")
 def Casting():
-    return "Casting"
+    return render_template("casting")
    
 if __name__ == "__main__":
     app.run(debug=True)
