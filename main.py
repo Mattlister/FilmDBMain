078182c73df61a82dbf32cf8cdec2c01c272e719
Matthew Lister <mdlister24@hotmail.com>
2020-01-11 13:02:54 +0000

    Added info to tv.html and tested change in main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route("/index")
+@app.route("/")
 def index ():
     return render_template("index.html")
 
-
 @app.route("/TV")
 def TV():
-    return "TV"
+    return render_template("tv.html")
 
 @app.route("/Casting")
 def Casting():
