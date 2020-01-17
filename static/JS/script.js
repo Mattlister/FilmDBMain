817b8cb06249b2bb40877acb03c8931cd9468113
Matthew Lister <mdlister24@hotmail.com>
2020-01-17 15:14:57 +0000

    Working on bg image changes

--- static/JS/script.js
+++ static/JS/script.js
@@ -2,8 +2,9 @@ $(document).ready(function) {
     Change background image of div when changing pages
     $("button").click(function() {
         var Image imageUrl = "../img/alien.jpg";
-        $(".box").css("background-image", "url("../img/alien.jpg")");
+        $(".box").css("background-image", "url(" + imageUrl + ")");
 
     });
 
 });
+}
