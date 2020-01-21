40386db89535dd91bcd88e0256d3ce873dd9dba0
Matthew Lister <mdlister24@hotmail.com>
2020-01-21 21:19:56 +0000

    Images added working on bg change link clicked

--- static/JS/script.js
+++ static/JS/script.js
@@ -3,25 +3,6 @@ const IMAGE URLS = {
     TV: 'https://da641f8a-74ec-4607-982a-5e597c0231a5.ws-eu01.gitpod.io/files/download/?id=a663aee9-9fa2-4300-a1a1-4cdaeb1e2bca',
 };
 
-// Sets the background image
-const setBackground = (image) => {
-    document.body.style.background = "url('"+IMAGE_URLS.[image]+"')";
-};
-
-if (isfilms) {
-    setBackground('films');
-} else {
-    setBackground('TV')
-}
-
-$(document).ready(() => {
-   $('#searchForm').on('submit', (e) => {
-      var searchText = $('#searchText').val();
-      getMovies(searchText);
-       e.preventDefault();
-   });
-});
-
 function getMovies(searchText){
   axios.get('http://www.omdbapi.com?s='+searchText)
     .then((response) => {
@@ -96,4 +77,12 @@ function getMovie(){
     .catch((err) => {
         console.log(err);
     });
+}
+
+}
+
+function changeImage(img){
+    
+    document.body.style.backgroundImage = 'url(http://placehold.it/'+../img/suits9.jpg++')';
+    
 }
