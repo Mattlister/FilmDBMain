4fb47995f66ee7537d34a5d8fa45706474e968d4
Matthew Lister <mdlister24@hotmail.com>
2020-01-21 18:46:41 +0000

    Changing the design and removing text

--- main.py
+++ main.py
@@ -1,54 +1,39 @@
-import os
-from os import path
-from flask import Flask, render_template, redirect, url_for, request
-import os
-from os import path
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask (__name__)
 
-
-@app.route('/index')
-def index():
+@app.route("/index")
+def index ():
     return render_template("index.html")
 
+@app.route("/films")
+def films ():
+    return render_template("films.html")    
 
-@app.route('/films')
-def films():
-    return render_template("films.html")
-
-
-@app.route('/tv')
+@app.route("/tv")
 def tv():
     return render_template("tv.html")
 
-
-@app.route('/casting')
+@app.route("/casting")
 def casting():
     return render_template("casting.html")
 
-
-@app.route('/discover')
+@app.route("/discover")
 def discover():
     return render_template("discover.html")
 
-
 @app.route("/filmdata")
 def filmdata():
-    return render_template("filmdata.html")
+    return render_template("filmdata.html")  
 
+@app.route("/login")
+def login():
+    return render_template("login.html")  
 
-@app.route('/signup', methods=('GET', 'POST'))
+@app.route("/signup")
 def signup():
-    signup_form = SignUpForm()
-    if signup_form.validate_on_submit():
+    return render_template("signup.html")  
 
 
- @app.route('/login', methods=('GET', 'POST'))
-  def login():
-    error = None
-    if request.method == 'POST':
-    if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-    error = 'Invalid Credentials. Please try again.'
-    else:
-    return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+if __name__ == "__main__":
+    app.run(debug=True)
