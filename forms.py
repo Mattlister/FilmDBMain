aded0a1b788aa18dad28f5ba98da643b9ff8e0db
Matthew Lister <mdlister24@hotmail.com>
2020-01-21 16:21:23 +0000

    Added new code for form submission

--- forms.py
+++ forms.py
@@ -10,7 +10,9 @@ username = StringField('Username', validators=[DataRequired()])
 password = PasswordField('Password', validators=[DataRequired()])
 submit = SubmitField('Login')
 
+
 class SignUpForm(FlaskForm)
+
 
 username = StringField('Username', validators=[DataRequired()])
 password = PasswordField('Password', validators=[DataRequired()])
--- main.py
+++ main.py
@@ -1,39 +1,54 @@
-from flask import Flask, render_template
+import os
+from os import path
+from flask import Flask, render_template, redirect, url_for, request
+import os
+from os import path
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route("/index")
-def index ():
+
+@app.route('/index')
+def index():
     return render_template("index.html")
 
-@app.route("/films")
-def films ():
-    return render_template("films.html")    
 
-@app.route("/tv")
+@app.route('/films')
+def films():
+    return render_template("films.html")
+
+
+@app.route('/tv')
 def tv():
     return render_template("tv.html")
 
-@app.route("/casting")
+
+@app.route('/casting')
 def casting():
     return render_template("casting.html")
 
-@app.route("/discover")
+
+@app.route('/discover')
 def discover():
     return render_template("discover.html")
 
+
 @app.route("/filmdata")
 def filmdata():
-    return render_template("filmdata.html")  
+    return render_template("filmdata.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")  
 
-@app.route("/signup")
+@app.route('/signup', methods=('GET', 'POST'))
 def signup():
-    return render_template("signup.html")  
+    signup_form = SignUpForm()
+    if signup_form.validate_on_submit():
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+ @app.route('/login', methods=('GET', 'POST'))
+  def login():
+    error = None
+    if request.method == 'POST':
+    if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    error = 'Invalid Credentials. Please try again.'
+    else:
+    return redirect(url_for('home'))
+    return render_template('login.html', error=error)
