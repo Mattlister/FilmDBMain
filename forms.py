c4d27b5e7105ac8a784585369e55ace89674456c
Matthew Lister <mdlister24@hotmail.com>
2020-01-23 16:39:26 +0000

    New form setup

--- forms.py
+++ forms.py
@@ -1,22 +1,21 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, DateField
-from wtforms.validators import DataRequired, Length, EqualTo
+from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length, AnyOf
 
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'DontTellAnyone'
 
-class LoginForm(FlaskForm)
+class LoginForm(Form):
+    username = StringField('username', validators=[InputRequired(), Email(message='I don\'t like your email.')])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=10), AnyOf(['secret', 'password']))]
 
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        form = LoginForm()
+        if form.validate_on_submit():
+            return 'Form Succesfully Submitted!'
+        return render_template('index.html', form=form) 
 
-username = StringField('Username', validators=[DataRequired()])
-password = PasswordField('Password', validators=[DataRequired()])
-submit = SubmitField('Login')
-
-
-class SignUpForm(FlaskForm)
-
-
-username = StringField('Username', validators=[DataRequired()])
-password = PasswordField('Password', validators=[DataRequired()])
-password_validate = PasswordField(
-     'Validate Password', validators=[DataRequired(), EqualTo('password', message='Passwords must be identical.')])
-    submit = SubmitField('Register')
-)
+if __name__ == '__main__':
+    app.run(debug=True)           
