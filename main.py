162fca3f562f247c2e74ab37ce10032076587824
Matthew Lister <mdlister24@hotmail.com>
2020-01-24 14:42:36 +0000

    Changing page location due to search function issue

--- main.py
+++ main.py
@@ -29,11 +29,6 @@ def signup():
     return render_template('pages/login.html')
 
 
-@app.route('/signup')
-def signup():
-    return render_template('pages/signup.html')  
-   
-
 
 if __name__ == "__main__":
     app.run(debug=True)
