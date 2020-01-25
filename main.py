016f6fbaa0f898a03269dbe212d1b9b36964ecf8
Matthew Lister <mdlister24@hotmail.com>
2020-01-25 09:04:09 +0000

    Added accidentally deleted data

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('pages/index.html')
+
+
+@app.route('/films')
+def films():
+    return render_template('pages/films.html')
+
+
+@app.route('/tv')
+def tv():
+    return render_template('pages/tv.html')
+
+
+@app.route('/casting')
+def casting():
+    return render_template('pages/casting.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('pages/login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('pages/signup.html')  
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
