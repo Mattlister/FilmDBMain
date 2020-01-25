15ecec55e83c08b266699e54e88b12618b13c227
Matthew Lister <mdlister24@hotmail.com>
2020-01-25 11:12:56 +0000

    Removed and edited none needed content

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('pages/index.html')
 
--- server.py
+++
@@ -1,2 +0,0 @@
-from flask_nav import Nav
-nav = Nav()
