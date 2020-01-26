9033a1c46f019cc71c2c58752e96712f61731b65
Matthew Lister <mdlister24@hotmail.com>
2020-01-26 16:04:45 +0000

    Signup issues, now resolved

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_pymongo import PyMongo
 from forms import SignUpForm
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    user_collection = mongo.db.users
-    user_collection.insert({'name : Matthew'})
-    return '<h1>Added a User!</h1>'
-
-app = Flask(__name__)
-app.comfig["MONGO_URI"] = os.environ.get('MONGO_URI')
-app.config['MONDOG_DBNAME'] = os.environ.get("MONGO_DBNAME")    
-app.config['SECRET_KEY'] = os.environ.get["SECRET_KEY"]
-
-mongo = PyMongo(app)
-users = mongo.db.users
 
 app = Flask(__name__)
 Bootstrap(app)
 
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thefilmDB'
 
 @app.route('/index')
 def index():
