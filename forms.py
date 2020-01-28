2b765d8c614b23edab35ad87475a9d592eec6fbb
Matthew Lister <mdlister24@hotmail.com>
2020-01-28 18:35:24 +0000

    Adding class to Forms

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, PasswordField, SelectField, BooleanField
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class SignUpForm(FlaskForm):
@@ -11,9 +11,21 @@ class SignUpForm(FlaskForm):
 class RegistratrionForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
+    password = PasswordField('New Password',
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
     accept_rules = BooleanField('I accept the site rules', [validators.InputRequired()])
+
+     submit = SubmitField('Register')
+
+     class CreateMovieInfoForm(FlaskForm):
+         submit = SubmitField('create')
+
+     class EditMovieInfoForm(FlaskForm):
+         submit = SubmitField('Update')
+
+
+    class DeleteForm(FlaskForm):
+        submit = SubmitField('Delete')
