368ac11c14d674ecf35c7d8fe5aa0dbeec9495fe
Matthew Lister <mdlister24@hotmail.com>
2020-02-02 12:14:52 +0000

    Removed due to security error

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("MONGO_URI", 'mongodb+srv://root:Qu4ntum2020!@myFirstCluster-qtxun.mongodb.net/FilmDB?retryWrites=true&w=majority')
-
-os.environ.setdefault("MONGO_DBNAME", 'FilmDB' )
-#Secret Key Value
-os.environ.setdefault("SECRET_KEY", 'Apple12345')
