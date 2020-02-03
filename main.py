a518e90ac0f5df2c40c55666e70b4ebf1e18cb72
Matthew Lister <mdlister24@hotmail.com>
2020-02-03 14:46:58 +0000

    More form changes to correct errors

--- main.py
+++ main.py
@@ -27,58 +27,108 @@ print(os.environ.get("MONGO_URI"))
 def index():
     return render_template("pages/index.html")
 
-
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    users = mongo.db.users
-    first_name = request.get_json()["first_name"]
-    last_name = request.get_json()["last_name"]
-    email = request.get_json()["email"]
-    password = bcrypt.generate_password_hash(request.get_json()["password"]).decode(
-        "utf-8"
-    )
-    created = datetime.utcnow()
-    user_id = users.insert(
-        {
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email,
-            "password": password,
-            "created": created,
-        }
-    )
-
-    new_user = users.find_one({"_id": user_id})
-
-    result = {"email": new_user["email"] + "registered"}
-
-    return jsonify({"result": result})
-
-
-@app.route("/login", methods=["GET", "POST"])
+# Login
+@app.route('/login', methods=['GET'])
 def login():
-    users = mongo.db.users
-    email = request.get_json()["email"]
-    password = request.get_json()["password"]
-    result = ""
+	# Check if user is not logged in already
+	if 'user' in session:
+		user_in_db = users_collection.find_one({"username": session['user']})
+		if user_in_db:
+			# If so redirect user to his profile
+			flash("You are logged in already!")
+			return redirect(url_for('profile', user=user_in_db['username']))
+	else:
+		# Render the page for user to be able to log in
+		return render_template("login.html")
 
-    response = users.find_one({"email": email})
+# Check user login details from login form
+@app.route('/user_auth', methods=['POST'])
+def user_auth():
+	form = request.form.to_dict()
+	user_in_db = users_collection.find_one({"username": form['username']})
+	# Check for user in database
+	if user_in_db:
+		# If passwords match (hashed / real password)
+		if check_password_hash(user_in_db['password'], form['user_password']):
+			# Log user in (add to session)
+			session['user'] = form['username']
+			# If the user is admin redirect him to admin area
+			if session['user'] == "admin":
+				return redirect(url_for('admin'))
+			else:
+				flash("You were logged in!")
+				return redirect(url_for('profile', user=user_in_db['username']))
+			
+		else:
+			flash("Wrong password or user name!")
+			return redirect(url_for('login'))
+	else:
+		flash("You must be registered!")
+		return redirect(url_for('register'))
 
-    if response:
-        if bcrypt.check_password_hash(response["password"], password):
-            access_token = create_access_token(
-                identity={
-                    "first_name": response["first_name"],
-                    "last_name": response["last_name"],
-                    "email": response["email"],
-                }
-            )
-            result = jsonify({"token": access_token})
-        else:
-            result = jsonify({"error": "Invalid username and password"})
-    else:
-        result = jsonify({"result": "No results found"})
-    return result
+# Sign up
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	# Check if user is not logged in already
+	if 'user' in session:
+		flash('You are already sign in!')
+		return redirect(url_for('index'))
+	if request.method == 'POST':
+		form = request.form.to_dict()
+		# Check if the password and password1 actualy match 
+		if form['user_password'] == form['user_password1']:
+			# If so try to find the user in db
+			user = users_collection.find_one({"username" : form['username']})
+			if user:
+				flash(f"{form['username']} already exists!")
+				return redirect(url_for('register'))
+			# If user does not exist register new user
+			else:				
+				# Hash password
+				hash_pass = generate_password_hash(form['user_password'])
+				#Create new user with hashed password
+				users_collection.insert_one(
+					{
+						'username': form['username'],
+						'email': form['email'],
+						'password': hash_pass
+					}
+				)
+				# Check if user is actualy saved
+				user_in_db = users_collection.find_one({"username": form['username']})
+				if user_in_db:
+					# Log user in (add to session)
+					session['user'] = user_in_db['username']
+					return redirect(url_for('profile', user=user_in_db['username']))
+				else:
+					flash("There was a problem savaing your profile")
+					return redirect(url_for('register'))
+
+		else:
+			flash("Passwords dont match!")
+			return redirect(url_for('register'))
+		
+	return render_template("register.html")
+
+# Log out
+@app.route('/logout')
+def logout():
+	# Clear the session
+	session.clear()
+	flash('You were logged out!')
+	return redirect(url_for('index'))
+
+# Profile Page
+@app.route('/profile/<user>')
+def profile(user): 
+	# Check if user is logged in
+	if 'user' in session:
+		# If so get the user and pass him to template for now
+		user_in_db = users_collection.find_one({"username": user})
+		return render_template('profile.html', user=user_in_db)
+	else:
+		flash("You must be logged in!")
+		return redirect(url_for('index'))
 
 
 @app.route("/films")
