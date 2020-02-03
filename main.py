5cf7c9e6ff1d971a3cb668e6723a165973d5336a
Matthew Lister <mdlister24@hotmail.com>
2020-02-03 14:26:07 +0000

    Changing login pages

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from os import path
 import os
-from flask import Flask, render_template, redirect, request, url_for,
-request
+import datetime
+from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
+from flask import jsonify
 from flask_bcrypt import Bcrypt
 
 if path.exists("env.py"):
-      import env # pylint: disable=W0611
+    import env # pylint: disable=W0611
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -18,9 +19,8 @@ mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 users = mongo.db.users
 
-# Collections
-
 print(os.environ.get("MONGO_URI"))
+
 
 @app.route("/")
 @app.route("/index")
@@ -28,38 +28,8 @@ def index():
     return render_template("pages/index.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    # Check to see if user is already logged in
-    if 'user' in session:
-        user_in_db = users_collection.find_one({"username":
-session['user']})
-        if user_in_db:
-            # If found, redirect user to their profile
-            flash("This is the page you are looking for!")
-            return redirect(url_for('profile', user=user_in_db['username']))
-    else:
-        # Display login page for the user        
-    return render_template("pages/login.html")
-
-#Sign up
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    # Check if new user
-    if 'user' in session:
-        flash('You know this place!')
-        return redirect(url_for('index'))
-    if request.method == 'POST':
-        form = request.form.to_dict()
-        # Check if passwords match
-        if form['user_password'] == form['user_password1']:
-            # If successful, find user in db
-            user = users_collection.find_one({"username" :
- form['username']})
-            if user:
-                flash(f"{form['username']} already exists!")
-                return redirect(url_for('register'))
-                    
     users = mongo.db.users
     first_name = request.get_json()["first_name"]
     last_name = request.get_json()["last_name"]
@@ -83,6 +53,32 @@ def signup():
     result = {"email": new_user["email"] + "registered"}
 
     return jsonify({"result": result})
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    users = mongo.db.users
+    email = request.get_json()["email"]
+    password = request.get_json()["password"]
+    result = ""
+
+    response = users.find_one({"email": email})
+
+    if response:
+        if bcrypt.check_password_hash(response["password"], password):
+            access_token = create_access_token(
+                identity={
+                    "first_name": response["first_name"],
+                    "last_name": response["last_name"],
+                    "email": response["email"],
+                }
+            )
+            result = jsonify({"token": access_token})
+        else:
+            result = jsonify({"error": "Invalid username and password"})
+    else:
+        result = jsonify({"result": "No results found"})
+    return result
 
 
 @app.route("/films")
@@ -111,7 +107,5 @@ def page_not_found(e):
     return render_template("pages/404.html"), 404
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+if __name__ == "__main__":
+    app.run(debug=False)
