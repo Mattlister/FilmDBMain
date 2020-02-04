174dec959b8299298f2c3ba5f5a1d38afbef601a
Matthew Lister <mdlister24@hotmail.com>
2020-02-04 19:48:13 +0000

    Cleaned up app.py whilst on cleaner forms mission

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import os
-from flask import Flask, render_template, url_for, request
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
 from flask_bcrypt import Bcrypt
 from os import path
-from wtforms.validators import InputRequired, Email, Length
 
-from os import path
+
 if path.exists("env.py"):
-  import env 
+    import env
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI') 
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 # Secret Key value
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -28,58 +24,20 @@ app = Flask(__name__)
 print(os.environ.get('MONGO_URI'))
 
 
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('pages/index.html')
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    users = mongo.db.users
-    first_name = request.get_json()['first_name']
-    last_name = request.get_json()['last_name']
-    email = request.get_json()['email']
-    password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
-    created = datetime.uctcnow()
-    
-    user_id = users.insert({
-        'first_name' : first_name,
-        'last_name' : last_name,
-        'email' : email,
-        'password' : password,
-        'created' : created,
-    })
-   
-    new_user = users.find_one({'_id' : user_id})
-
-    result = {'email' : new_user['email'] + 'registered'}
-
-    return jsonify({'result' : result})
+    return render_template('pages/signup.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    users = mongo.db.users
-    email = request.get_json()['email']
-    password = request.get_json()['password']
-    result = ""
-
-    response = users.find_one({'email' : email})
-
-    if response:
-        if bcrypt.check_password_hash(response['password'], password):
-            access_token = create_access_token(identity = {
-            'first_name': response['first_name'],
-            'last_name': response['last_name'],
-            'email': response['email']}
-            )
-            result = jsonify({"token": access_token})
-        else:
-            result = jsonify({"error":"Invalid username and password"})
-    else:
-            result = jsonify({"result":"No results found"})
-    return result
+    return render_template('pages/login.html')
 
 
 @app.route('/films')
@@ -96,6 +54,7 @@ def tv():
 def casting():
     return render_template('pages/casting.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('pages/contact.html')
@@ -106,5 +65,6 @@ def contact():
 def page_not_found(e):
     return render_template('pages/404.html'), 404
 
-if __name__ ==  "__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
