31b24a342bfc5d9ffa4eb892f49d202a2bd9fcbc
Matthew Lister <mdlister24@hotmail.com>
2020-02-04 21:53:59 +0000

    No errors in forms.py

--- templates/components/forms/forms.py
+++ templates/components/forms/forms.py
@@ -7,14 +7,14 @@ class RegistrationForm(FlaskForm):
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
 
 
-email = StringField("Email", validators=[DataRequired(), Email()])
-password = PasswordField("Password", validators=[DataRequired()])
-confirm_password = PasswordField("Confirm Password",
-                            Validators=[DataRequired(), EqualTo('password')])
-
-submit = SubmitField('Sign Up')
+submit = SubmitField("Sign Up")
 
 
 class LoginForm(FlaskForm):
@@ -22,7 +22,7 @@ class LoginForm(FlaskForm):
 
 
 password = PasswordField("Password", validators=[DataRequired()])
-remember = BooleanField('Remember Me')
+remember = BooleanField("Remember Me")
 
 
-submit = SubmitField('Login')
+submit = SubmitField("Login")
