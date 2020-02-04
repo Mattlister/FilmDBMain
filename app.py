5539a6a7b0b79469c9dc760c8cd8ce09be00a53d
Matthew Lister <mdlister24@hotmail.com>
2020-02-04 20:01:30 +0000

    More code added and cleaned up errors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, url_for
+from forms import RegistratrionForm, LoginForm
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
@@ -10,10 +11,23 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 # Secret Key value
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-
 bcrypt = Bcrypt(app)
 mongo = PyMongo(app)
 
+posts = [
+    {
+        'user': 'Matthew Lister',
+        'title': 'First movie entry',
+        'content': 'Empire Sucks',
+        'date_posted': 'February 4, 2020'
+    },
+    {
+        'user': 'Samantha Lister',
+        'title': 'First TV entry',
+        'content': 'Loving Suits',
+        'date_posted': 'February 5, 2020'
+    }
+]
 
 app = Flask(__name__)
 print(os.environ.get('MONGO_URI'))
@@ -25,15 +39,27 @@ def index():
     return render_template('pages/index.html')
 
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    return render_template('pages/signup.html')
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegistratrionForm()
+    return render_template('pages/register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('pages/login.html')
+    form = LoginForm()
+    return render_template('pages/login.html', title='Login', form=form)
 
+
+
+
+
+
+
+
+
+
+"""
 
 @app.route('/films')
 def films():
@@ -54,7 +80,6 @@ def casting():
 def contact():
     return render_template('pages/contact.html')
 
-
 # 404 error page
 @app.errorhandler(404)
 def page_not_found(e):
@@ -63,3 +88,4 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    """
