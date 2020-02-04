c7f39d5246159268e636a173568289f449a06f14
Matthew Lister <mdlister24@hotmail.com>
2020-02-04 23:24:47 +0000

    Finished login connection in app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def index():
     return render_template('pages/index.html')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -49,6 +49,13 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirecr(url_for('home'))
+        else:
+            flash('Login Unsuccessful, please check username and password', 'danger')
+
     return render_template('pages/login.html', title='Login', form=form)
 
 
