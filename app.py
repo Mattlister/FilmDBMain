60efca2b27499b593b8bc4bfb86a1a68a418eccd
Matthew Lister <mdlister24@hotmail.com>
2020-02-05 10:09:14 +0000

    Resolving new app.py errors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 # Secret Key value
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -14,76 +14,73 @@ bcrypt = Bcrypt(app)
 
 posts = [
     {
-        'user': 'Matthew Lister',
-        'title': 'First movie entry',
-        'content': 'Empire Sucks',
-        'date_posted': 'February 4, 2020'
+        "user": "Matthew Lister",
+        "title": "First movie entry",
+        "content": "Empire Sucks",
+        "date_posted": "February 4, 2020",
     },
     {
-        'user': 'Samantha Lister',
-        'title': 'First TV entry',
-        'content': 'Loving Suits',
-        'date_posted': 'February 5, 2020'
-    }
+        "user": "Samantha Lister",
+        "title": "First TV entry",
+        "content": "Loving Suits",
+        "date_posted": "February 5, 2020",
+    },
 ]
 
 app = Flask(__name__)
-print(os.environ.get('MONGO_URI'))
+print(os.environ.get("MONGO_URI"))
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('pages/index.html')
+    return render_template("pages/index.html")
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!')
+        flash('')
         return redirect(url_for('home'))
-    return render_template('pages/register.html', title='Register', form=form)
+    return render_template("pages/register.html", title="Register", form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com':
-            if form.password.data == 'password':
-                flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        if form.email.data == "admin@blog.com":
+            if form.password.data == "password":
+                flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
         else:
-            flash('Login Unsuccessful, please check username and password',           'danger')
+            flash('error')
 
-    return render_template('pages/login.html', title='Login', form=form)
+    return render_template("pages/login.html", title="Login", form=form)
 
 
-@app.route('/films')
+@app.route("/films")
 def films():
-    return render_template('pages/films.html')
+    return render_template("pages/films.html")
 
 
-@app.route('/tv')
+@app.route("/tv")
 def tv():
-    return render_template('pages/tv.html')
+    return render_template("pages/tv.html")
 
 
-@app.route('/casting')
+@app.route("/casting")
 def casting():
-    return render_template('pages/casting.html')
+    return render_template("pages/casting.html")
 
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
-    return render_template('pages/contact.html')
+    return render_template("pages/contact.html")
+
 
 # 404 error page
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('pages/404.html'), 404
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("pages/404.html"), 404
