e92e5b8310ae0d911dfe68159944e16ec314d10b
Matthew Lister <mdlister24@hotmail.com>
2020-02-05 16:58:32 +0000

    Changes made to correct syntax error

--- app.py
+++ app.py
@@ -50,14 +50,12 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'mdlister24@hotmail.com'
-        and form.password.data == 'password':
+        if form.email.data == 'mdlister24@hotmail.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful, please check username
                   and password', 'danger')
-
     return render_template('pages/login.html', title='Login', form=form)
 
 
