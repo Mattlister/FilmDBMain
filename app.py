ed94e4ef5b303a8996c9d553b260c1f74e034a04
Matthew Lister <mdlister24@hotmail.com>
2020-02-05 14:59:11 +0000

    One error to fix

--- app.py
+++ app.py
@@ -37,27 +37,28 @@ def index():
     return render_template("pages/index.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('')
+        flash(f'Account created for {form.username.data}!', 'suucess')
         return redirect(url_for('home'))
-    return render_template("pages/register.html", title="Register", form=form)
+    return render_template('pages/register.html', title='Register', form=form)
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "admin@blog.com":
-            if form.password.data == "password":
-                flash("You have been logged in!", "success")
-            return redirect(url_for("home"))
+        if form.email.data == 'mdlister24@hotmail.com'
+        and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
         else:
-            flash('error')
+            flash('Login Unsuccessful, please check username
+                  and password', 'danger')
 
-    return render_template("pages/login.html", title="Login", form=form)
+    return render_template('pages/login.html', title='Login', form=form)
 
 
 @app.route("/films")
