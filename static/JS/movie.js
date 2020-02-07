a1f5b8eb756b20d3ac1878204e66dec0deb3ef9d
Matthew Lister <mdlister24@hotmail.com>
2020-02-07 14:08:08 +0000

    Working movie search function

--- static/JS/movie.js
+++ static/JS/movie.js
@@ -1,4 +1,5 @@
-var data;
+<script>
+							var data;
 function getanswer(q){
 $.get("https://www.omdbapi.com/?s="+q+"&apikey=be4b405e", function(rawdata){    
  let rawstring =JSON.stringify(rawdata);
@@ -8,4 +9,6 @@ $.get("https://www.omdbapi.com/?s="+q+"&apikey=be4b405e", function(rawdata){
  let imdburl="https://www.imdb.com/title/"+data.Search[0].imdbID+"/";
 
  let posterurl =data.Search[0].Poster;
-document.getElementById('answer').innerHTML="<h1>"+title+"</h1><br> <img src= '"+posterurl+"'><br><p> Year Released:"+year+"</p> <br> <p> IMDB page: <a href='"+imdburl+"'target='_blank'>"+imdburl+"</a></p>"; }); }
+document.getElementById('answer').innerHTML="<h1>"+title+"</h1><br> <img src= '"+posterurl+"'><br><p> Year Released:"+year+"</p> <br> <p> IMDB page: <a href='"+imdburl+"'target='_blank'>"+imdburl+"</a></p>"; })
+}
+</script>
