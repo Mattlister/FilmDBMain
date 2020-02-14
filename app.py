076b415c758b7f9ee4bfe755c8f25e6e33e0600b
Matthew Lister <mdlister24@hotmail.com>
2020-02-14 20:59:36 +0000

    Resolving app.py issues

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from os import path
 
-if path.exists("env.py"):
+if path.exists('env.py'):
     import env
-
+    
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -15,6 +15,7 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+users = mongo.db.users
 
 
 posts = [
