75cc7aa71ce233805a47b5f41c477930af597349
Matthew Lister <mdlister24@hotmail.com>
2020-03-23 08:19:47 +0000

    No changes made, removed entry planned on using

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 
 
 class RegistrationForm(FlaskForm):
