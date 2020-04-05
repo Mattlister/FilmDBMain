05548b0c4f1b8269db1f85f6cda36e7099e0b72a
Matthew Lister <mdlister24@hotmail.com>
2020-04-05 14:41:44 +0000

    Working on forms

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 
 
 class RegistrationForm(FlaskForm):
@@ -15,8 +15,6 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField(
-        "Username", validators=[DataRequired(), Length(min=2, max=20)]
-    )
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Login")
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
