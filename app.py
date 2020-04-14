a0a7b25d1a76b7221505f2e99df05f3251814f69
Matthew Lister <mdlister24@hotmail.com>
2020-04-14 20:28:04 +0000

    Page changes to py files

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if os.path.exists('env.py'):
 
 app = Flask(__name__)
 
-AllMovies = AllMovies()
+allmovies = AllMovies()
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
--- data.py
+++ data.py
@@ -2,21 +2,21 @@ def AllMovies():
     allmovies = [
         {
             'id': 1,
-            'title': 'Movie One',
+            'title': 'editmovie One',
             'body': 'Text goes here',
             'author': 'Matthew Lister',
             'create_date': '10-04-2020'
         },
         {
             'id': 2,
-            'title': 'Movie Two',
+            'title': 'editmovie Two',
             'body': 'Text goes here',
             'author': 'John Doe',
             'create_date': '10-04-2020'
         },
         {
             'id': 3,
-            'title': 'Movie Three',
+            'title': 'editmovie Three',
             'body': 'Text goes here',
             'author': 'James Lister',
             'create_date': '10-04-2020'
